ebf17d27e8d6763a9930e2e31f3d6433c2484fce
Andrew Brown <andrew.brown@qub.ac.uk>
2025-04-07 20:45:16 +0100

    Added some error handling
    Got a weird error if one of the students submitted xls instead of xlsx,
    so have added a handler for that,
    
    also now added a try except so that one faulty group/spreadsheet doesn't
    torpedo the whole thing.

--- PeerMark/extract_peer.py
+++ PeerMark/extract_peer.py
@@ -8,6 +8,9 @@ def read_command_line():
                         default=None)
     parser.add_argument('-s', '--semester', help='semester id, e.g. "2241"',
                         default=None)
+    parser.add_argument('-d', '--dryrun', help="don't edit canvas grades, store them in marks.csv",
+                        action='store_true',
+                        default=False)
 
     args = parser.parse_args()
     args.assignment = True
@@ -50,21 +53,30 @@ def extract_data(fname):
     return df
 
 
-def writedata(df, subs):
+def writedata(df, subs, args):
     """given a data frame containing all the collected comments for a given group, pull out the comments for each member, write them to a file, then calculate the mark awarded and update the canvas submission with the mark and comments"""
     import numpy as np
     from canvas_selector import nameFile, update_grade
+    from pathlib import Path
 
     for sub in subs:
         fname = nameFile(sub)
         df_ = df[sub.name].copy()
         df_.columns = ['' for _ in range(len(df_.columns))]
 
+        fpath = Path(fname)
+        if fpath.suffix != '.xlsx':
+            fname = fpath.with_suffix('.xlsx')
+
         df_.to_excel(fname)
         df_ = df_.replace(np.nan, 7)
         mark = np.round(10*np.average(df_.iloc[[len(df_)-1]]),0)
-        update_grade(sub, mark)
-        sub.upload_comment(fname)
+        if args.dryrun:
+            with open('marks.csv', 'a') as f:
+                f.write(f"{sub.name}, {mark}\n")
+        else:
+           update_grade(sub, mark)
+           sub.upload_comment(fname)
 
 
 def grade_submissions(args, group_dict):
@@ -79,11 +91,15 @@ def grade_submissions(args, group_dict):
         for sub in subs:
             fname = nameFile(sub)
             if os.path.isfile(fname):
-                tmpdf = extract_data(fname)
-                fulldf = pd.concat([fulldf, tmpdf], axis=1)
+                try:
+                    tmpdf = extract_data(fname)
+                    fulldf = pd.concat([fulldf, tmpdf], axis=1)
+                except:
+                    print(f"problem reading {fname} from {sub.name}")
+                    pass
 
         fulldf.to_csv('op.csv')
-        writedata(fulldf, subs)
+        writedata(fulldf, subs, args)
 
 
 def main():
@@ -91,7 +107,8 @@ def main():
     args = read_command_line()
     group_dict = download_submissions(args)
     grade_submissions(args, group_dict)
-    cleanup(args.ass.id)
+    if not args.dryrun:
+        cleanup(args.ass.id)
 
 
 if __name__ == '__main__':
